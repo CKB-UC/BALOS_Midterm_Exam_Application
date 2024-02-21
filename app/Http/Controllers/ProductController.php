cc355840fe9966d105af9575011092b94407cf99
Balos <Baloskeanu@gmail.com>
2024-02-21 09:42:27 +0800

    creating the controller and defining products.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = [
+            ['name' => 'One Piece', 'Demon Slayer', 'Attack on Titan', 'Food Wars', 'Golden Kamui' ]
+        ];
+        return view('products',compact('products'));
+    }
 }
